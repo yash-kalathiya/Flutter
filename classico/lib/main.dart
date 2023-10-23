60caa95a40ad23745c527a6bf516e35dba01451b
yash-kalathiya <kalathiyayash163@gmail.com>
2023-10-23 13:15:25 +0530

    second

--- classico/lib/main.dart
+++ classico/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "First App",
       home: Container(
-        color: Colors.amber,
+        color: Colors.blue,
       ),
     );
   }
